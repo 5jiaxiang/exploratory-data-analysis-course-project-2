939342b9abed4e7370f0ac6dff5295d2b5cab196
5jiaxiang <144208641+5jiaxiang@users.noreply.github.com>
2023-09-11 19:45:06 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,21 +1,15 @@
-# Load the NEI & SCC data frames.
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+##readData
 
-# Subset NEI data by Baltimore's fip.
-baltimoreNEI <- NEI[NEI$fips=="24510",]
+SubNEI<-subset(NEI,fips=="24510")
+aggNEI<-aggregate(Emissions~year,SubNEI,sum)
+##subset only year and emission and sum emissions of the same year together
 
-# Aggregate using sum the Baltimore emissions data by year
-aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
 
-png("plot2.png",width=480,height=480,units="px",bg="white")
-
-barplot(
-  aggTotalsBaltimore$Emissions,
-  names.arg=aggTotalsBaltimore$year,
-  xlab="Year",
-  ylab="PM2.5 Emissions (Tons)",
-  main="Total PM2.5 Emissions From all Baltimore City Sources"
-)
+barplot((aggNEI$Emissions)/10^2,names.arg = aggNEI$year,
+        xlab="year",ylab="emissions(10^2 tons)",main="Total Emissions of PM 2.5 in Baltimore City ")
+##PLOT
 
 dev.off()
