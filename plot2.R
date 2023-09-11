46e90d1e32bf0dce0631b293ce6504af13d44d09
5jiaxiang <144208641+5jiaxiang@users.noreply.github.com>
2023-09-11 19:46:14 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,15 +1,15 @@
+
+## Loading required libraries
+library(plyr); library(grDevices)
+
+#Loading the main dataset
 NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-##readData
+# Obtaining summary dataset for Baltimore by getting the total emissions for each yeari using fips=="24510" argument
+NEI_sum_Balt <- ddply(filter(NEI,fips=="24510"), .(year), summarize, sum=sum(Emissions))
 
-SubNEI<-subset(NEI,fips=="24510")
-aggNEI<-aggregate(Emissions~year,SubNEI,sum)
-##subset only year and emission and sum emissions of the same year together
-
-png("plot2.png",width=480,height=480,units="px",bg="transparent")
-
-barplot((aggNEI$Emissions)/10^2,names.arg = aggNEI$year,
-        xlab="year",ylab="emissions(10^2 tons)",main="Total Emissions of PM 2.5 in Baltimore City ")
-##PLOT
-
-dev.off()
+# plotting a barchart for total emissions in Baltimore for each year
+png(filename = "plot2.png", height = 600, width = 600)  
+p <- barplot(height=NEI_sum_Balt[,2], name=NEI_sum[,1],
+             main=" Total PM2.5 emission from all sources in Baltimore ", ylab="PM25 Emission", xlab="year", ylim=c(0,4000))
+text (x=p,y=NEI_sum_Balt[,2], label=format(round(NEI_sum_Balt[,2],1)),pos=1)
+dev.off()      
